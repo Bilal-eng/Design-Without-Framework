6d4d826d34446d8a5024966e96b909052385b98e
Bilal Sheikh Khalil <bilalsheikhkhalil@gmail.com>
2019-04-08 00:06:31 +0300

    add responsive navigation bar

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,25 @@ $(function(){
         $('.info-content div').hide();
         $('.' + $(this).data('class')).fadeIn();
     });
+
+    $('.links li a').click(function(){
+
+        $('html, body').animate({
+
+            scrollTop: $('#' + $(this).data('value')).offset().top
+        }, 1000);
+
+    });
+
+    $('.links li a').click(function(){
+
+        $(this).parent().addClass('active').siblings().removeClass('active');
+
+    });
+
+    $('.faicon .icon').click(function(){
+
+        $('.navbar .links').toggleClass('hidden-sm').toggleClass('hidden-xs');
+
+    });
 });
